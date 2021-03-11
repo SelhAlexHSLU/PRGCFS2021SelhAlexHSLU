d6c005d986e5e9bc73a348701d316a532e03547e
prgc <alex.selhofer@stud.hslu.ch>
2021-03-11 20:24:04 +0100

    aktuell

---
+++ 40_Exercises/21_Workshops/forLoops.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	int sum = 0;
+	int start = 2;
+	int end = 10;
+	
+	for( int i = start; i <= end 1++)
+	{
+		sum = sum +i;
+	}
+	
+	printf("
+
+
+	return 0;
+}
