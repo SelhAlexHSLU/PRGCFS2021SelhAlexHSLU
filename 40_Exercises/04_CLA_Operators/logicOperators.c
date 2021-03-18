417910e76f380928990fd070a35dbf5c69f6eb1b
prgc <alex.selhofer@stud.hslu.ch>
2021-03-18 20:31:19 +0100

    18.03.

--- 40_Exercises/04_CLA_Operators/logicOperators.c
+++ 40_Exercises/04_CLA_Operators/logicOperators.c
@@ -1,18 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	//falls NICHT 3 Argumente erhalten dann --> Block 7 bis 10
-	if ( argc !=3){
+	//falls NICHT 4 Argumente erhalten dann --> Block 7 bis 10
+	if ( argc !=4)
+	{
 		//input not valid
 		printf("Usage:\n");
-		printf("logicOperators Input1 Input2\n");
+		printf("logicOperators Input1 Input2 Input3\n");
 	}
 	else
-	{	//input valid-->genau 3 Argumente erhalten
-		printf("Number of input arguments: %d\n", argc);// anzahl Args printen
+	{	//input valid-->genau 1+3 Argumente erhalten
+		int val1 = atoi(argv[1]);
+		int val2 = atoi(argv[2]);
+		int val3 = atoi(argv[3]);
+		int sum = 0;
+		float average = 0.0f;
+		printf("%d, %d, %d\n", val1, val2, val3);
 		
-		//die drei Elemente in der For-Loop Deklaration
+		if( !(val1 > 0) || !(val2 > 0) || !(val3 > 0) )
+		{
+			//Fehlerfall
+			printf("All inputs must be strictly greater than zero.\n");
+		}	
+		else
+		{
+			//find the greatest
+			if( val1 >= val2 && val1 >= val3)
+			{
+				printf("%d is the greatest\n", val1);
+			}
+			else if( val2 >= val1 && val2 >= val3)
+			{
+				printf("%d is the greatest\n", val2);
+			}
+			else
+			{
+				printf("%d is the greatest\n", val3);
+			}
+			//find the smallest
+			if( val1 <= val2 && val1 <= val3)
+			{
+				printf("%d is the smallest\n", val1);
+			}
+			else if( val2 <= val1 && val2 <= val3)
+			{
+				printf("%d is the smallest\n", val2);
+			}
+			else
+			{
+				printf("%d is the smallest\n", val3);
+			}
+		}
+		
+		{
+		//compute the sum
+		sum = (val1 + val2 + val3);
+		printf("the sum is: %d\n", sum);
+		
+		//compute the average
+		average = (sum/3);
+		printf("the average is: %f\n", average);
+		}
+		
+		
+	}	
+#if 0	//auskommentieren mit Präprozessor if(
+	//die drei Elemente in der For-Loop Deklaration
 		//nimm eine int-variable i und starte mit 0
 		// dann führe folgenden Code aus solange i kleiner ist als argc
 		//inkrementier i bei jeden Durchlauf um 1 (++).
@@ -25,6 +80,8 @@ int main(int argc, char *argv[])
 		//		-34
 		}
 	}
+#endif
+
 	return 0;
 }
 
--- 40_Exercises/21_Workshops/forLoops.c
+++ 40_Exercises/21_Workshops/forLoops.c
@@ -2,17 +2,52 @@
 
 int main (int argc, char *argv[])
 {
-	int sum = 0;
-	int start = 2;
+
+	int start = 0;
 	int end = 10;
 	
-	for( int i = start; i <= end 1++)
+	for( int i = start; i < end; i++)
+
 	{
-		sum = sum +i;
+		for( int k = start+1; k <= end; k++)
+		{
+		printf("%d ", i*10+k);
+		}
+		printf("\n");
 	}
+	/*
+	int start = 1;
+	int end = 729;
 	
-	printf("
+	for( int i = start; i <= end; i = i * 3)
+	{
+		printf("%d\n", i);
+	}
+	*/
+	
+	/*
+	int a;
 
-
+	for( a = 40 ; a >= 25 ; a = a - 1)
+	{
+	printf("%d\n", a);
+	}
+	*/
+	
+	/*
+	int sum = 0;
+	int start = 40;
+	int end = 25;
+	
+	for( int i = start; i >= end; i++ )
+	{
+		sum = sum + i;
+	
+	/*
+	printf("The sum from %d to %d is equal to %d\n", start, end, sum);
+	
+	printf("%d\n", sum);
+	}
+	*/
 	return 0;
 }
